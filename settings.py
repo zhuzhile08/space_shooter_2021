1a782f2d449af48459fbcc35e2b24e1e8ec67c9c
zhuzhile08 <zhuzhile08@gmail.com>
2021-07-21 20:43:45 +0200

    add function that can automatically generate list with all the enemies for a stage

--- settings.py
+++ settings.py
@@ -14,3 +14,21 @@ enemyFolder = os.path.join(imgFolder, 'Enemies')
 
 kenFont = pygame.font.Font(os.path.join('data', 'kenvector_future_thin.ttf'), 30)     # font
 
+
+# generate contents of the enemy dictionaries
+
+
+def generateEnemyDictionaryStage1():
+    stg1EnemyList = []
+    for i in range(100):
+        change = random.randint(0, 10)
+        if 0 <= change <= 4:
+            enemy = obj.Meteor(1, os.path.join(enemyFolder, 'meteorBrown1.png'), (random.randrange(200, 450), -200 * i, random.randrange(-5, 5), random.randrange(4, 8)), 0, 0)
+        elif 5 <= change <= 9:
+            enemy = obj.Meteor(1, os.path.join(enemyFolder, 'meteorGrey1.png'), (random.randrange(200, 450), -200 * i, random.randrange(-5, 5), random.randrange(4, 8)), 0, 0)
+        else:
+            enemy = obj.Meteor(1, os.path.join(enemyFolder, 'ufoGrey.png'), (random.randrange(200, 450), -200 * i, random.randrange(-5, 5), random.randrange(4, 8)), 0, 0)
+
+        stg1EnemyList.append(enemy)
+
+    return stg1EnemyList
